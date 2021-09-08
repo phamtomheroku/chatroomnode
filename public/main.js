95c3139cb9f033e5034fa612d6cf07f8314de3b5
phamtomheroku <82068532+phamtomheroku@users.noreply.github.com>
2021-09-07 20:14:35 -0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -276,3 +276,29 @@ $(function() {
   });
 
 });
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  padding: 25px;
+  background-color: white;
+  color: black;
+  font-size: 25px;
+}
+
+.dark-mode {
+  background-color: black;
+  color: white;
+}
+</style>
+</head>
+<body>
+
+<button onclick="myFunction()">Toggle Dark Mode</button>
+
+<script>
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+</script>
