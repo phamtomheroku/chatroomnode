3c5dd01ba02c8ab651ee7956a09eb86fb28f1c3a
phamtomheroku <82068532+phamtomheroku@users.noreply.github.com>
2021-09-07 19:59:03 -0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -225,7 +225,7 @@ $(function() {
   socket.on('login', (data) => {
     connected = true;
     // Display the welcome message
-    const message = 'Welcome to Socket.IO Chat – ';
+    const message = 'Pain ';
     log(message, {
       prepend: true
     });
